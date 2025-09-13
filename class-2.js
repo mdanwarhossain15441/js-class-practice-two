552f2ae0484ba12108a1d33827c58954b9f3f846
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2025-09-14 00:10:05 +0600

    I complite task 4

--- class-2.js
+++ class-2.js
@@ -46,7 +46,7 @@ if (BMI<18.5){
     console.log("you are obese");
 }
 
-// task 
+// task 3
 /***
 
 Grade Calculator
@@ -75,4 +75,33 @@ if(studentNum>=90 && studentNum<=100){
     console.log("your value is not right");
 }else{
     console.log("you are fail");
+}
+
+// task 
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+
+var myScore = 80;
+var friendScore = 97;
+
+if (myScore>=80){
+    if(friendScore>=80){
+        console.log("let's go to a lunch");
+    }else if(friendScore<80 && friendScore>=60){
+        console.log("good luck next time");
+    }else if(friendScore<60 && friendScore>=40){
+        console.log("I will not seen my friend message");
+    }else if(myfriend<40){
+        console.log("my friend fail so i have to hide from his so it's batter is I have to take a deep sleep");
+    }
 }
