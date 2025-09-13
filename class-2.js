60349b9211b00f24f053e034343c2e1f5d8bcd72
Md. Anwar Hossain <aymankajolanwar@gmail.com>
2025-09-14 01:10:43 +0600

    class 2 task complite

--- class-2.js
+++ class-2.js
@@ -105,3 +105,63 @@ if (myScore>=80){
         console.log("my friend fail so i have to hide from his so it's batter is I have to take a deep sleep");
     }
 }
+
+// task 
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+var number1 = 50;
+var number2 = 30;
+
+var result;
+
+if(number1>number2){
+    console.log(number1*2);
+}else{
+    console.log(number1+number2);
+}
+
+// in tarnary opatator
+
+var tarnaryNum1 =56;
+var tarnaryNum2 =70;
+
+var result = (tarnaryNum1>tarnaryNum2) ? (tarnaryNum1*2) : (tarnaryNum1+tarnaryNum2);
+
+console.log(result);
+
+// task 6
+
+/***
+
+Ticket fare Calculator
+    - Children (age < 10): free
+    - Students get a 50% discount
+    - Senior citizens (age >= 60) gets a 15% Discount
+    - Otherwise Regular ticket fare 800 tk
+*/
+
+var age = 34;
+var isStudent = false;
+var fair = 800;
+
+
+if (age<=10){
+    console.log("As you are a children so your fair is free");
+}else if(isStudent){
+    console.log("As you are a student so you have to give us 1/2 fair that's means " + fair/2 + " Tk");
+}else if(age>=60){
+    console.log("As you are a senior citizen that's why you have to give fair is" + (fair/100)*85 + "TK");
+}else{
+    console.log("our Bus fair is 800 Tk only")
+}
